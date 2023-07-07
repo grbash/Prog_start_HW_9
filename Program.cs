d51ba8c93310cef4b045f03e43b73d795664cb95
grbash <grbash@gmail.com>
2023-07-07 14:58:53 +0300

    Task68

--- Program.cs
+++ Program.cs
@@ -1,4 +1,27 @@
 ﻿// Допущение для всех задач: Пользователь вводит адекватные значение 
 // (если в условии задачи написано "возводит число A в натуральную степень B", то мы допускаем, 
-// что число B на вход мы получим натуральное, а вот А может быть любое, иначе придется использовать слишком много проверок)
+// что число B на вход мы получим натуральное, а вот А может быть любое, иначе придется использовать слишком много проверок
 
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.WriteLine("Task 68:");
+Console.WriteLine();
+
+int Akkerman(int m, int n)
+{
+
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return Akkerman(n,m);
+
+}
+
+Console.Write("Enter M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int res = Akkerman(m, n);
+Console.Write(res);
